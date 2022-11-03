0073283689be7bbe78868bfc1f04fbb8c13be127
Pablodos-Hub <pablodos.99@gmail.com>
2022-11-02 21:46:34 -0300

    actualizando pc

--- Tp_macowins/descontinuar_productos.py
+++ Tp_macowins/descontinuar_productos.py
@@ -1,13 +1,16 @@
+from time import strftime
 from macowins import *
 from datetime import datetime
 
 from persistencia import cargar_todos
 
-# ...
-        
+import subprocess
+
+subprocess.call(["zenity", "--info",  '--title=Hola', "--text=¡Hola mundo!", "--display=:0"])
+
+print ("hola pablo")
+print("Descontinuando productos sin stock")
+
 if __name__ == "__main__":
     for sucursal in cargar_todos():
         sucursal.descontinuar_productos()
-
-        
-    print(datetime.now() + "Descontinuando productos sin stock")
