35519d4ac6b2578460a89c00e0339f3fe598895c
Pablodos-Hub <pablodos.99@gmail.com>
2022-11-13 15:00:58 -0300

    descontinuar productos

--- Tp_macowins/descontinuar_productos.py
+++ Tp_macowins/descontinuar_productos.py
@@ -3,8 +3,6 @@ from macowins import *
 def main():
   print(str(datetime.now()) + " : descontinuando productos")
   sucursales = cargar_todos()
-  print (sucursales)
-  print (cargar_todos())
   for sucursal in sucursales.keys():
       sucu = cargar(sucursal)
       sucu.descontinuar_productos()
