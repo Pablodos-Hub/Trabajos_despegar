15ff81ba93aede434d1036602298c23e34f37d3b
Pablodos-Hub <pablodos.99@gmail.com>
2022-11-28 16:57:45 -0300

    modificando archivo main a index y agregando productos a la pagina

--- Tp_macowins/app.py
+++ Tp_macowins/app.py
@@ -4,8 +4,54 @@ from macowins import *
 app = Flask(__name__)
 
 @app.get("/")
-def main():
-    return render_template("main.html")
+def index():
+    return render_template("index.html",productos = articulos())
+
+
+
+def articulos():
+    for producto in retiro.productos:
+        if producto.codigo == 100:
+            articulo1 = producto
+        
+        if producto.codigo == 110:
+            articulo2 = producto
+                
+        if producto.codigo == 120:
+            articulo3 = producto
+                
+        if producto.codigo == 130:
+            articulo4 = producto
+                
+        if producto.codigo == 140:
+            articulo5 = producto
+                
+        if producto.codigo == 150:
+            articulo6 = producto
+                
+        if producto.codigo == 200:
+            articulo7 = producto
+                
+        if producto.codigo == 210:
+            articulo8 = producto
+                
+        if producto.codigo == 220:
+            articulo9 = producto
+                
+        if producto.codigo == 230:
+            articulo10 = producto
+                
+        if producto.codigo == 240:
+            articulo11 = producto
+                
+        if producto.codigo == 250:
+            articulo12 = producto
+    productos  = [articulo1,articulo2,articulo3,articulo4,articulo5,articulo6,articulo7,articulo8,articulo9,articulo10,articulo11,articulo12]        
+    return productos
+                 
+        
+    
+           
 
 @app.get("/pagina2")
 def pagina2():
