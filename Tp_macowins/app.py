1f538ad670e31122e1023e2295bc18a36372f52e
Pablodos-Hub <pablodos.99@gmail.com>
2022-11-27 21:51:20 -0300

    re-haciendo el tp de 0,nuevo estilo

--- Tp_macowins/app.py
+++ Tp_macowins/app.py
@@ -9,5 +9,24 @@ def main():
 
 @app.get("/pagina2")
 def pagina2():
-    return render_template("pagina2.html")    
+    return render_template("pagina2.html",nombre =nombre())  
+
+
+def nombre():
+    return request.args.get("nombre","")      
        
+@app.get("/pagina3")
+def pagina3():
+    return render_template("pagina3.html",categoria =categoria())  
+
+
+def categoria():
+    return request.args.get("categoria","")
+
+@app.get("/pagina4")
+def pagina4():
+    return render_template("pagina4.html",precio =precio())  
+
+
+def precio():
+    return request.args.get("precio","")    
